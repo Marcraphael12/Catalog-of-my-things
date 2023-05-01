1deeb1f79fefb978aeac6bc683e055d59ce12771
rica213 <25f5k252l@mozmail.com>
2023-05-01 18:43:59 +0300

    Implement all visible methods on diagram

--- classes/author.rb
+++ classes/author.rb
@@ -1,2 +1,15 @@
 class Author
+  attr_reader :id, :items
+  attr_accessor :first_name, :last_name
+
+  def initialize(first_name, last_name)
+    @id = Random.rand(1..2000)
+    @first_name = first_name
+    @last_name = last_name
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+  end
 end
--- classes/genre.rb
+++ classes/genre.rb
@@ -1,3 +1,14 @@
 class Genre
+  attr_reader :id, :items
+  attr_accessor :name
 
+  def initialize(name)
+    @id = Random.rand(1..2000)
+    @name = name
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+  end
 end
