b1e7f1529e295212d02e6d427175984e6930c03f
rica213 <25f5k252l@mozmail.com>
2023-05-02 20:49:31 +0300

    Implement the method that add music album

--- classes/app.rb
+++ classes/app.rb
@@ -1,12 +1,19 @@
+require_relative './music'
+
 class Startup
+  def initialize
+    @music_albums = []
+  end
+
   # the user options
   def options_list
+    puts ''
     puts 'Please choose an option according to the numbers on the dashboard:
     1# List all books
     2# List all music albums
     3# List of games
     4# Add a book
-    5# Add a music
+    5# Add a music album
     6# Add a game
     7# Exit'
 
@@ -18,7 +25,7 @@ class Startup
   def selection(choice)
     methods = [
       method(:booklist), method(:musiclist), method(:gamelist),
-      method(:create_book), method(:create_music), method(:create_game), method(:quit_app)
+      method(:create_book), method(:add_music_album), method(:create_game), method(:quit_app)
     ]
 
     # according to the number entered we call the defined method
@@ -47,9 +54,19 @@ class Startup
     puts 'create book'
   end
 
-  # TODO: To be implemented later
-  def create_music
-    puts 'create music'
+  def add_music_album
+    puts '_____ ADD A NEW MUSIC ALBUM _____'
+    puts ''
+    print 'Is it on Spotify? (y/n) '
+    on_spotify_input = gets.chomp.downcase
+    on_spotify = %w[y yes].include?(on_spotify_input)
+    print 'What is the date of publication? (DD/MM/YYYY) '
+    publish_date = gets.chomp
+    print 'Is it archived? (y/n) '
+    archived_input = gets.chomp.downcase
+    archived = %w[y yes].include?(archived_input)
+    @music_albums << MusicAlbum.new(on_spotify, publish_date, archived)
+    puts 'A music album is created successfully'
   end
 
   # TODO: To be implemented later
