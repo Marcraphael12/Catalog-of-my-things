c067896fa08453757aa881f2db9f1babda2cda14
Marc raphael <marcraphael979@gmail.com>
2023-05-02 15:06:53 +0100

    Setup can_be_archive method and call publish_date from the constructor

--- classes/game_development.rb
+++ classes/game_development.rb
@@ -7,8 +7,15 @@ class Game < Item
   # Parameters:
   # multiplayer: boolean
   # last_played_at: date
-  def initialize(multiplayer, last_played_at)
+  def initialize(multiplayer, last_played_at, publish_date)
+    # Calls the parent class constructor
+    super publish_date
     @multiplayer = multiplayer
     @last_played_at = last_played_at
+  end
+
+  # We check if the game can be archive or not
+  def can_be_archived?
+    super && (@last_played.year < Date.today.year - 2)
   end
 end
