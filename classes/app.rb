4a9f69f82c160379db90679fe669fac30aa814ce
Marc raphael <marcraphael979@gmail.com>
2023-05-02 10:03:55 +0100

    Add the dashboard

--- classes/app.rb
+++ classes/app.rb
@@ -1,5 +1,69 @@
-class App
-  def initialize
-    @any_param = []
+class Startup
+  # the user options
+  def options_list
+    puts 'Please choose an option according to the numbers on the dashboard:
+    1# List all books
+    2# List all music albums
+    3# List of games
+    4# Add a book
+    5# Add a music
+    6# Add a game
+    7# Exit'
+
+    choice = gets.chomp
+    selection(choice.to_i)
+  end
+
+  # We save our selection into an array
+  def selection(choice)
+    methods = [
+      method(:booklist), method(:musiclist), method(:gamelist),
+      method(:create_book), method(:create_music), method(:create_game), method(:quit_app)
+    ]
+
+    # according to the number entered we call the defined method
+    (1..7).include?(choice) && methods[choice - 1].call
+  end
+
+  # Our dashboade methods
+  # Defined with default options
+  # TODO: To be implemented later
+  def booklist
+    puts 'book list in library'
+  end
+
+  # TODO: To be implemented later
+  def musiclist
+    puts 'Music list in library'
+  end
+
+  # TODO: To be implemented later
+  def gamelist
+    puts 'Game list in library'
+  end
+
+  # TODO: To be implemented later
+  def create_book
+    puts 'create book'
+  end
+
+  # TODO: To be implemented later
+  def create_music
+    puts 'create music'
+  end
+
+  # TODO: To be implemented later
+  def create_game
+    puts 'create game'
+  end
+
+  def quit_app
+    puts 'Thanks for using our app'
+    exit
+  end
+
+  # Then we start the app
+  def start
+    options_list
   end
 end
