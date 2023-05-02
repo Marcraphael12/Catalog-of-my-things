825825d73935f703ac5a203c7e5081afed47fa7c
Marc raphael <marcraphael979@gmail.com>
2023-05-02 15:42:30 +0100

    Fix typo

--- classes/item.rb
+++ classes/item.rb
@@ -14,22 +14,22 @@ class Item
     @archived = archived
   end
 
-  def add_genre(_genre)
+  def add_genre(genre)
     @genre = genre
     genre.items << self unless genre.items.include?(self)
   end
 
-  def add_source(_source)
+  def add_source(source)
     @source = source
     source.items << self unless source.items.include?(self)
   end
 
-  def add_author(_author)
+  def add_author(author)
     @author = author
     author.items << self unless author.items.include?(self)
   end
 
-  def add_label(_label)
+  def add_label(label)
     @label = label
     label.items << self unless label.items.include?(self)
   end
