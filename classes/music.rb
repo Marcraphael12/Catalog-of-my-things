6974e5db481d884c61fcf78d832acd5a17dde5fa
rica213 <25f5k252l@mozmail.com>
2023-05-02 14:04:28 +0300

    Override the method can_be_archived?

--- classes/music.rb
+++ classes/music.rb
@@ -3,8 +3,13 @@ require_relative './item'
 class MusicAlbum < Item
   attr_accessor :on_spotify, :publish_date
   attr_reader :id, :archived
-  def initialize (on_spotify, publish_date, archived)
+
+  def initialize(on_spotify, publish_date, _archived)
     super(publish_date, archived: true)
     @on_spotify = on_spotify
+  end
+
+  def can_be_archived?
+    Date.today.prev_year(10) > @publish_date && _on_spotify
   end
 end
