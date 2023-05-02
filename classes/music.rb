77aed9c8a810054c6197220aea9cecd8c4fd6b89
rica213 <25f5k252l@mozmail.com>
2023-05-02 13:51:45 +0300

    Add getters and setters in the music class

--- classes/music.rb
+++ classes/music.rb
@@ -1,8 +1,10 @@
 require_relative './item'
 
 class MusicAlbum < Item
+  attr_accessor :on_spotify, :publish_date
+  attr_reader :id, :archived
   def initialize (on_spotify, publish_date, archived)
-    super(publish_date, archived)
+    super(publish_date, archived: true)
     @on_spotify = on_spotify
   end
 end
