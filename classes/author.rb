d4977237288fc961db1876582cff907a663cc567
Marc raphael <marcraphael979@gmail.com>
2023-05-02 16:12:40 +0100

    import parent constructor from item

--- classes/author.rb
+++ classes/author.rb
@@ -1,6 +1,4 @@
-class Author
-  attr_reader :id, :items
-  attr_accessor :first_name, :last_name
+require_relative './item'
 
   def initialize(first_name, last_name)
     @id = Random.rand(1..2000)
