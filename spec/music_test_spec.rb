c4b08e884267fc72a94ee375ead990dd87e5828e
rica213 <25f5k252l@mozmail.com>
2023-05-02 18:32:58 +0300

    Separate unit tests for music and genre

--- spec/music_test_spec.rb
+++ spec/music_test_spec.rb
@@ -5,7 +5,9 @@ describe MusicAlbum do
   before(:context) do
     @music_album_one = MusicAlbum.new(true, '7/10/2028', false)
     @music_album_two = MusicAlbum.new(false, '21/11/2000', false)
-    @music_album_three = MusicAlbum.new(true,'13/12/2010', false)
+    @music_album_three = MusicAlbum.new(true, '13/12/2010', false)
+    @indie = Genre.new('indie')
+    @rock = Genre.new('rock')
   end
   context 'Testing all the methods in MusicAlbum' do
     it 'Should be an instance of MusicAlbum' do
@@ -27,24 +29,5 @@ describe MusicAlbum do
       expect(@music_album_three.move_to_archive).to eq true
       expect(@music_album_three.archived).to eq true
     end
-    
-    it 'Should add genre to the music album and update the number of items to the Genre class' do
-      indie = Genre.new('indie')
-      @music_album_one.add_genre(indie)
-      expect(@music_album_one.genre).to eq indie
-      expect(indie.items).to include @music_album_one
-    end
-
-    it 'Should add music album in Genre class and prevent to add a music album if its genre is already defined' do
-      rock = Genre.new('rock')
-      indie = Genre.new('indie')
-      @music_album_one.add_genre(indie)
-      rock.add_item(@music_album_one)
-      rock.add_item(@music_album_two)
-      rock.add_item(@music_album_three)
-      expect(@music_album_one.genre).to eq indie
-      expect(@music_album_two.genre).to eq rock
-      expect(rock.items).to include @music_album_two && @music_album_three
-    end
-  end  
+  end
 end
