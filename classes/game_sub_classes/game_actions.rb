a8224caa90c8dd43e142f25c9431fa7c8e8c709e
Marc raphael <marcraphael979@gmail.com>
2023-05-03 13:45:07 +0100

    Initialize GameActions class

--- classes/game_sub_classes/game_actions.rb
+++ classes/game_sub_classes/game_actions.rb
@@ -2,4 +2,14 @@ require './game_development'
 require './author'
 
 class GameActions
+  # Make class properties accessible
+  attr_accessor :games, :authors
+
+  # Class initialization
+  # games: assigned to the method load_games(from the json file) 
+  # authors: assigned to the method load_authors(from the json file)
+  def initialize
+    @games = load_games
+    @authors = load_authors
+  end
 end
