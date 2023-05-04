cb95e700339d9cab4b4f3072f649dfd98839a01a
Marc raphael <marcraphael979@gmail.com>
2023-05-04 01:04:02 +0100

    Create new author objects

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,2 +1,9 @@
 require_relative '../classes/game_development'
 require_relative '../classes/item'
+
+# Settings:
+# we first create new authors obects
+before :each do
+	@author1 = Author.new 'Marc', 'Raphael'
+	@author2 = Author.new 'Clarielle', 'Larissa'
+end
