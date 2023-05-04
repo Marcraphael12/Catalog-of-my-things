a67b9c82c37a59e6c7ba6797ef98dd72d52b9e8a
Marc raphael <marcraphael979@gmail.com>
2023-05-04 01:47:15 +0100

    Check if last_played and publish_date are correctly set

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,7 +5,7 @@ describe Game do
   # Settings:
   # we first create a new game object
   before :each do
-    @game = Game.new true, '2010-10-10', '202"-11-11'
+    @game = Game.new true, '2010-10-10', '2005-11-11'
   end
 
   # Test 1:
@@ -23,6 +23,25 @@ describe Game do
   describe 'Multiplayer input testing' do
     it 'returns the correct value for Multiplayer' do
       expect(@game.multiplayer).to be eql true
+    end
+  end
+
+  # Test 3:
+  # This is a test case that checks if the `last_played_at` attribute of the `Game` class is set
+  # correctly. It expects the `last_played_at` attribute of the `@game` object to be equal to
+  # `'2010-10-10'`.
+  describe 'Last played date check' do
+    it 'returns the correct value for last played date' do
+      expect(@game.last_played_at).to be eql '2010-10-10'
+    end
+  end
+
+  # This is a test case that checks if the `publish_date` attribute of the `Game` class is set
+  # correctly. It expects the `publish_date` attribute of the `@game` object to be equal to
+  # `'2005-11-11'`.
+  describe 'Published date date check' do
+    it 'returns the correct value for publish date' do
+      expect(@game.publish_date).to be eql '2005-11-11'
     end
   end
 end
