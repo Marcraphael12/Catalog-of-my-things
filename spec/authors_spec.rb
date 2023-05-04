c71cf5573a7dfd21a30faf480da773dc5e75cf27
Marc raphael <marcraphael979@gmail.com>
2023-05-04 08:51:47 +0100

    Create new author object

--- spec/authors_spec.rb
+++ spec/authors_spec.rb
@@ -1,2 +1,9 @@
 require_relative '../classes/author'
 require_relative '../classes/item'
+
+describe Author do
+  before :each do
+    @author1 = Author.new 'Marc', 'raphael'
+    @author2 = Author.new 'Clarielle', 'larissa'
+  end
+end
