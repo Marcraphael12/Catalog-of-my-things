525caf64f3719cc84106578ed79c039215137dc8
Marc raphael <marcraphael979@gmail.com>
2023-05-04 09:45:34 +0100

    Test add_item method: it adds a new item to an author

--- spec/authors_spec.rb
+++ spec/authors_spec.rb
@@ -5,6 +5,7 @@ describe Author do
   before :each do
     @author1 = Author.new 'Marc', 'raphael'
     @author2 = Author.new 'Clarielle', 'larissa'
+    @new_item = Item.new '2023-05-04'
   end
 
   describe 'initialization of a new author' do
@@ -14,11 +15,18 @@ describe Author do
     end
 
     it 'returns the correct value for first name of @author1' do
-      expect(@author1.firts_name).to eql 'Marc'
+      expect(@author1.first_name).to eql 'Marc'
     end
 
     it 'returns the correct value for last name of @author2' do
       expect(@author2.last_name).to eql 'larissa'
+    end
+  end
+
+  describe 'add_item method test' do
+    it 'should add new author item' do
+      @author1.add_item(@new_item)
+      expect(@author1.items).to eql([@new_item])
     end
   end
 end
