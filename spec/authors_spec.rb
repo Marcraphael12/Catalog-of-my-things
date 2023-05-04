e59e43236491f0ad4fe2fd1babf1589fcf589842
Marc raphael <marcraphael979@gmail.com>
2023-05-04 09:19:46 +0100

    Check if @author first_name and last_name are correctly set

--- spec/authors_spec.rb
+++ spec/authors_spec.rb
@@ -6,4 +6,19 @@ describe Author do
     @author1 = Author.new 'Marc', 'raphael'
     @author2 = Author.new 'Clarielle', 'larissa'
   end
+
+  describe 'initialization of a new author' do
+    it 'takes three parameters and returns 2 author objects' do
+      expect(@author1).to be_an_instance_of Author
+      expect(@author2).to be_an_instance_of Author
+    end
+
+    it 'returns the correct value for first name of @author1' do
+      expect(@author1.firts_name).to eql 'Marc'
+    end
+
+    it 'returns the correct value for last name of @author2' do
+      expect(@author2.last_name).to eql 'larissa'
+    end
+  end
 end
