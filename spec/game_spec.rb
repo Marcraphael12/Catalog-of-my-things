195f9566e367dbc88d6804eb9be91a65702fb065
Marc raphael <marcraphael979@gmail.com>
2023-05-04 01:39:58 +0100

    Check if the `multiplayer` attribute of the `Game` class is set

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -13,7 +13,16 @@ describe Game do
   # `@game` object is an instance of the `Game` class.
   descriibe 'Create a new game object' do
     it 'Takes 3 arguments and return the game object' do
-      expect(@game).to be an_instance_of
+      expect(@game).to be an_instance_of Game
+    end
+  end
+
+  # Test 2:
+  # This is a test case that checks if the `multiplayer` attribute of the `Game` class is set
+  # correctly. It expects the `multiplayer` attribute of the `@game` object to be equal to `true`.
+  describe 'Multiplayer input testing' do
+    it 'returns the correct value for Multiplayer' do
+      expect(@game.multiplayer).to be eql true
     end
   end
 end
