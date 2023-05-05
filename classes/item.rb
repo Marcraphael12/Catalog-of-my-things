708dd0071555ad55e786f89836c71be25162ef0d
Marc raphael <marcraphael979@gmail.com>
2023-05-05 13:05:08 +0100

    Run rubocop: Fix linters

--- classes/item.rb
+++ classes/item.rb
@@ -27,7 +27,6 @@ class Item
 
   ##
   # This function adds the current item to the author's list of items if it is not already included.
-  # 
   # Args:
   #   author: The parameter "author" is a variable that represents an instance of the Author class. It
   # is passed as an argument to the "add_author" method.
@@ -41,9 +40,9 @@ class Item
     label.items << self unless label.items.include?(self)
   end
 
-##
-# This Ruby function checks if an item can be archived based on its publish date being more than 10
-# years ago.
+  ##
+  # This Ruby function checks if an item can be archived based on its publish date being more than 10
+  # years ago.
   def can_be_archived?
     @publish_date.to_i < Date.today.year - 10
   end
