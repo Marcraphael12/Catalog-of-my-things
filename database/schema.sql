570dfaaf1b4e6e927e462c9fd0a664612402b03e
Marc raphael <marcraphael979@gmail.com>
2023-05-05 13:25:31 +0100

    Combine sql tables into one file

--- database/schema.sql
+++ database/schema.sql
@@ -12,3 +12,19 @@ create table if not exists item (
   -- reference to the author table
   foreign key (author_id) references authors (id)
 );
+
+create table Game (
+  multiplayer BOOLEAN,
+  last_played_at DATE
+)
+
+create table Authors (
+  id  INT generated always as identity,
+  first_name varchar(500),
+  last_name varchar(500),
+  primary key(id),
+
+-- a reference to the item table
+  foreign key(id) references item(id)
+)
+
